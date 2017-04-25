3b184d1b78d5eabbe4324617aac7560cfa671c78
walter hurtado <walter.hurtado@lkmx.io>
2017-04-25 13:38:52 -0700

    alloe-yarn

---
+++ site/web/app/themes/alloe-yarn/resources/views/partials/features.blade.php
@@ -0,0 +1,38 @@
+<div class="not-just-app">
+    <div class="description">
+        <h1 class="title">It’s not just an app</h1>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
+    </div>
+    <div class="feature social">
+        <h2>Social</h2>
+        <svg width="65px" height="7px" viewBox="-2 78 65 7" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <path d="M0.5,81.5 L60.6664352,81.5" id="Line" stroke="#6277DA" stroke-width="3" stroke-linecap="square" fill="none"></path>
+        </svg>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+        <div class="img"></div>
+    </div>
+    <div class="feature engagement">
+        <h2>Engagement</h2>
+        <svg width="65px" height="7px" viewBox="-2 78 65 7" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <path d="M0.5,81.5 L60.6664352,81.5" id="Line" stroke="#6277DA" stroke-width="3" stroke-linecap="square" fill="none"></path>
+        </svg>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+        <div class="img"></div>
+    </div>
+    <div class="feature management">
+        <h2>Management</h2>
+        <svg width="65px" height="7px" viewBox="-2 78 65 7" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <path d="M0.5,81.5 L60.6664352,81.5" id="Line" stroke="#6277DA" stroke-width="3" stroke-linecap="square" fill="none"></path>
+        </svg>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+        <div class="img"></div>
+    </div>
+    <div class="feature measurement">
+        <h2>Measurement</h2>
+        <svg width="65px" height="7px" viewBox="-2 78 65 7" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <path d="M0.5,81.5 L60.6664352,81.5" id="Line" stroke="#6277DA" stroke-width="3" stroke-linecap="square" fill="none"></path>
+        </svg>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+        <div class="img"></div>
+    </div>
+</div>
