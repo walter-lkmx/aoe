433770b49aace19ebe905ad5b56b47696cf70648
walter hurtado <walter.hurtado@lkmx.io>
2017-04-25 19:06:12 -0700

    update

--- site/web/app/themes/alloeYarn/resources/views/template-custom.blade.php
+++ site/web/app/themes/alloeYarn/resources/views/template-custom.blade.php
@@ -1,5 +1,5 @@
 {{--
-  Template Name: Custom Template
+  Template Name: Front Page
 --}}
 
 @extends('layouts.app')
@@ -7,6 +7,11 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
-    @include('partials.content-page')
+    @include('partials.welcome')
+    @include('partials.our-numbers')
+    @include('partials.not-just-app')
+    @include('partials.simple-as')
+    @include('partials.our-advocates')
+    {{-- @include('partials.content-page') --}}
   @endwhile
-@endsection
+@endsection 
